aa945a537655733b44ef8d37f04147935052e0cf
ashutosh-4 <ashutosh.chauhan_cs18@gla.ac.in>
2020-11-24 22:09:47 +0530

    added variablees

--- app/src/main/java/com/pranav/stepbooster/Database.java
+++ app/src/main/java/com/pranav/stepbooster/Database.java
@@ -2,7 +2,13 @@ package com.pranav.stepbooster;
 
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Database extends SQLiteOpenHelper
 {
+    private final static String DB_NAME = "steps";
+    private final static int DB_VERSION = 2;
 
+    private static Database instance;
+    private static final AtomicInteger openCounter = new AtomicInteger();
 }
